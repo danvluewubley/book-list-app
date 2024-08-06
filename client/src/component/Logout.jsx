b1b7c99dc79018bbb96d9ac80a12890f8954ac74
Daniel Wu <danielw72908@gmail.com>
2024-08-06 17:14:37 -0400

    Updated authentication after logout

--- client/src/component/Logout.jsx
+++ client/src/component/Logout.jsx
@@ -1,14 +1,18 @@
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { useEffect } from "react";
+import { useAuth } from './AuthContext'; // Adjust the import path
 
 function Logout() {
+  const { logout } = useAuth();
+
   const navigate = useNavigate();
 
   useEffect(() => {
-    const logout = async () => {
+    const handleLogout = async () => {
       try {
         const response = await axios.post("http://localhost:8080/logout");
+        logout();
         navigate("/");
       } catch (error) {
         if (error.response) {
@@ -19,8 +23,8 @@ function Logout() {
       }
     };
 
-    logout();
-  }, [navigate]);
+    handleLogout();
+  }, [navigate, logout]);
 
   return null;
 }
